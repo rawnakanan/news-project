87b285af61fb96f9bd6a87766185662ce147420a
rawnakanan <rawnakanan59989@gmail.com>
2023-12-06 14:39:19 +0600

    first commit

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,41 @@
+<div class="navbar-bg"></div>
+
+{{-- NavBar Start --}}
+
+@include('admin.layouts.navbar')
+
+{{-- NavBar End --}}
+
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="active">
+                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                {{-- <ul class="dropdown-menu">
+          <li class=active><a class="nav-link" href="index-0.html">General Dashboard</a></li>
+          <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
+        </ul> --}}
+            </li>
+            <li class="menu-header">Starter</li>
+            {{-- Single  --}}
+            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+            {{-- dropdown --}}
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Forms</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="forms-advanced-form.html">Advanced Form</a></li>
+                    <li><a class="nav-link" href="forms-editor.html">Editor</a></li>
+                    <li><a class="nav-link" href="forms-validation.html">Validation</a></li>
+                </ul>
+            </li>
+
+        </ul>
+    </aside>
+</div>
